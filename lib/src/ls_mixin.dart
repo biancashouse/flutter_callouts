d4577c53c8d7ceab47126b6e447104b740a72934
Bianca Cat <git@biancashouse.com>
2025-08-23 11:45:06 +1000

    release 7.4.1

--- lib/src/ls_mixin.dart
+++ lib/src/ls_mixin.dart
@@ -3,6 +3,8 @@ import 'package:flutter/foundation.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
+import '../flutter_callouts.dart' show fca;
+
 
 mixin LocalStorageMixin {
   Storage get localStorage {
@@ -15,6 +17,7 @@ mixin LocalStorageMixin {
           ? HydratedStorageDirectory.web
           : HydratedStorageDirectory((await getTemporaryDirectory()).path),
     );
-    //fca.initGotits();
+    fca.initGotits();
+
   }
 }
