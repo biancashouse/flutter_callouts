5a28f41f2f8e3b8c012d9857f7a3f28a558ec986
Bianca Cat <git@biancashouse.com>
2025-08-23 16:05:12 +1000

    refactor main

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,19 +1,35 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_callouts/flutter_callouts.dart';
 
 import 'callouts_intro.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() {
+  runZonedGuarded<Future<void>>(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
 
-  fca.logger.d('Running example app');
-  fca.loggerNs.i('Info message');
-  fca.loggerNs.w('Just a warning!');
-  fca.logger.e('Error! Something bad happened', error: 'Test Error');
-  fca.loggerNs.t({'key': 5, 'value': 'something'});
+      fca.logger.d('Running example app');
+      fca.loggerNs.i('Info message');
+      fca.loggerNs.w('Just a warning!');
+      fca.logger.e('Error! Something bad happened', error: 'Test Error');
+      fca.loggerNs.t({'key': 5, 'value': 'something'});
 
-  await fca.initLocalStorage();
+      await fca.initLocalStorage();
 
-  runApp(const FlutterCalloutsApp(title: 'flutter_callouts demo', home: IntroPage()));
+      runApp(
+        const FlutterCalloutsApp(
+          title: 'flutter_callouts demo',
+          home: IntroPage(),
+        ),
+      );
+    },
+    (Object error, StackTrace stack) {
+      // Handle the error here, e.g., log it, show a dialog, etc.
+      print('Caught error in runZonedGuarded: $error');
+      print('Stack trace: $stack');
+      // Optionally, send the error to a crash reporting service like Sentry or Firebase Crashlytics
+    },
+  );
 }
-
